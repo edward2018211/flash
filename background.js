aec7806391d38e04e204f961da4de9dac612e815
edwhuang <edwhuang@umich.edu>
2019-06-03 16:57:03 +0800

    changes

--- background.js
+++ background.js
@@ -127,8 +127,6 @@ class Garage {
 
 // Runs when Chrome Extension clicked on
 chrome.browserAction.onClicked.addListener(function(tab) {
-
-
     var links = ["https://mail.google.com/mail/u/0/#inbox", "https://www.instagram.com", 
     "https://www.facebook.com/", "https://www.reddit.com/r/uofm/new/", "https://www.google.com/search?q=michigan+football+recruiting", 
     "https://www.google.com/search?q=michigan+basketball"];
@@ -136,5 +134,5 @@ chrome.browserAction.onClicked.addListener(function(tab) {
     pocket.setName = "Social Pocket";
     pocket.addSetOfURL(links);
     pocket.flashPocket();
-    pocket.dullPocket();
+    //pocket.dullPocket();
 });
